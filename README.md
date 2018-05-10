049ca4665c78b4ac2276d9d91965fb83b95a8bd5
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2018-05-11 01:08:40 +0530

    Add script to set proxy in kgp

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# shell_scripting
-Practice for learning shell scripting. [Tutorial](https://www.shellscript.sh/first.html)
+# LINUX Proxy KGP 
+
+The script in the repository sets proxy for almost everything in the
+Ubuntu/Debian system.
+
+-- Gnome system wise
+-- Apt Package Installer
+-- Environment 
+-- Export to terminal
+
+## How to run
+
+- Clone the repository
+- `sudo chmod +x set_proxy_kgp.sh && sudo ./set_proxy_kgp.sh`
+- Preferably restart the system, or at least restart the terminal session.
+
+## Test
+
+- Open `www.google.com` in browser
+- In terminal `sudo apt-get install curl`
+- In terminal `curl www.google.com`
+
+If all the above steps pass, congratulations.
+For any quries, contant github@thealphadollar
---
+++ set_proxy_kgp.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+# A script to set system-wide proxy in Ubuntu / Debian
+# for KGP junta
+# created by thealphadollar
+
+PROXY_HOST=172.16.2.30
+PROXY_PORT=8080
+
+# setting system wide proxy
+gsettings set org.gnome.system.proxy mode manual
+gsettings set org.gnome.system.proxy.http host "$PROXY_HOST"
+gsettings set org.gnome.system.proxy.http port "$PROXY_PORT"
+gsettings set org.gnome.system.proxy.https host "$PROXY_HOST"
+gsettings set org.gnome.system.proxy.https port "$PROXY_PORT"
+gsettings set org.gnome.system.proxy.ftp host "$PROXY_HOST"
+gsettings set org.gnome.system.proxy.ftp port "$PROXY_PORT"
+gsettings set org.gnome.system.proxy.socks host "$PROXY_HOST"
+gsettings set org.gnome.system.proxy.socks port "$PROXY_PORT"
+
+
+# setting apt proxy
+
+## in apt.conf
+sudo sed -i.bak '/http[s]::proxy/Id' /etc/apt/apt.conf
+sudo tee -a /etc/apt/apt.conf <<EOF
+Acquire::http::proxy "http://${PROXY_HOST}:${PROXY_PORT}/";
+Acquire::https::proxy "http://${PROXY_HOST}:${PROXY_PORT}/";
+EOF
+
+## in apt.conf.d/70debconf
+sudo sed -i.bak '/http[s]::proxy/Id' /etc/apt/apt.conf.d/70debconf
+sudo tee -a /etc/apt/apt.conf.d/70debconf <<EOF
+Acquire::http::proxy "http://${PROXY_HOST}:${PROXY_PORT}/";
+Acquire::https::proxy "http://${PROXY_HOST}:${PROXY_PORT}/";
+Acquire::ftp
+ {
+   Proxy "ftp://${PROXY_HOST}:{PROXY_PORT}/";
+   ProxyLogin
+   {
+      "USER $(SITE_USER)@$(SITE)";
+      "PASS $(SITE_PASS)";
+   }
+ }
+EOF
+
+
+# setting environment proxy
+sudo sed -i.bak '/http[s]_proxy/Id' /etc/environment
+sudo tee -a /etc/environment <<EOF
+http_proxy="http://${PROXY_HOST}:${PROXY_PORT}/"
+https_proxy="http://${PROXY_HOST}:${PROXY_PORT}/"
+EOF
+
+
+# proxy for profile
+sudo touch /etc/profile.d/proxy.sh
+sudo tee -a /etc/profile.d/proxy.sh <<EOF
+export http_proxy="http://${PROXY_HOST}:${PROXY_PORT}"
+export https_proxy="http://${PROXY_HOST}:${PROXY_PORT}"
+export HTTP_PROXY="http://${PROXY_HOST}:${PROXY_PORT}"
+export HTTPS_PROXY="http://${PROXY_HOST}:${PROXY_PORT}"
+EOF
