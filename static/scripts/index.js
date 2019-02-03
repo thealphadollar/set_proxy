8eaacc533b94b344fb6817a45b10049fcfe68d14
baymac <parichay.barpanda@gmail.com>
2019-02-03 06:14:58 +0530

    Modified instructions format and changed colors

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,6 +1,4 @@
-// $(.button).hover(function() {
-//   $(.button_hover).css({'margin: -20px 0 0 0 0'});
-// });
+
 $(document).ready(function() {
   $(".button a").hover(function() {
      $(".bottom").slideDown(500);
