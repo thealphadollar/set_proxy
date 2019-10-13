669af7b149502df05a5a975e916555d08d077db7
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2019-10-13 23:53:27 +0530

    fix download link

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/download')
+def download():
+    return app.send_static_file('set_proxy.sh')
+
 
 if __name__ == '__main__':
     app.debug=True
